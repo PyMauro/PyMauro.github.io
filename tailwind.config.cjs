a965dcba85f1a7e3e823fd8f868d89976deed794
Mauro Sandes <mauroesandes@gmail.com>
2023-01-06 17:37:05 -0300

    safety safe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: theme => ({
+				'wave-pattern': "url('../public/img/wave-white.png')",
+				'wave-pattern-dots': "url('../public/img/pattern-white-dots.png')",
+				'wave-pink': "url('../public/img/wave-pink')"
+			})
+		},
+		screens:{
+			'xsm':'390px'
+		}
 	},
-	plugins: [],
+	plugins: [
+
+	],
 }
